416b5b693e2362c722e9e88c50d24d54c908f8d9
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-05-18 22:45:28 +0200

    🐛 fix(bashrc): make sure snipets are included only once
    We wrap all in a hierarchy of helper functions to determine the actual
    order of all the snipets. The functions can be run only once, so this takes
    care of the redundency. At the end we concatenate in the determined
    order all the snipets, resulting in the right .bashrc.

--- env-manager/build-env.bash
+++ env-manager/build-env.bash
@@ -52,18 +52,6 @@ set-avatar() {
     fi
 }
 
-include-bashrc-sources() {
-    sed -i -E "s:\\\$AVATAR:$AVATAR:;s%^\s*(\.|source)\s+(\S+)$% \
-        if [ -f $BASHRC_DIR/\2 ]; then \
-            cat $BASHRC_DIR/\2; \
-        elif [ -f \2 ]; then \
-            cat \2; \
-        else \
-            echo echo $1: unresolved \2 '>\&2'; \
-        fi \
-    %e" $1
-}
-
 make-index-files() {
     local escaped_pattern=$(sed "s:/:\\\\/:g" <<< $BASHRC_DIR)
 
@@ -74,14 +62,86 @@ make-index-files() {
     | xargs -i sh -c "make-index-file {}; echo . {}/index.bashrc >> $MAIN_FILE"
 }
 
-make-tmp-bashrc() {
-    echo "" > "$PREVIOUS_TMP_BASHRC_FILE"
-    cp "$MAIN_FILE" "$TMP_BASHRC_FILE"
+preprocess-source-file() {
+    # Make sure $AVATAR is always resolved.
+    # Replace all inclusions with a magic mangled string used elsewhere
+    # to build wrapper run-only-once funcs.
+    # This is a trick to determine in which order to include snipets
+    if [ -f $1 ]; then
+        cat $1 | sed -E "s:\\\$AVATAR:$AVATAR:;s%^(\s*)(\.|source)\s+(\S+)$% \
+            if [ -f $BASHRC_DIR/\3 ]; then \
+                echo '\1'\\\$(mangle-name $BASHRC_DIR/\3 $SCRIPT_DIR); \
+            elif [ -f \3 ]; then \
+                echo '\1'\\\$(mangle-name \3 $SCRIPT_DIR); \
+            else \
+                echo echo $1: unresolved \3 '>\&2'; \
+            fi; \
+        %e"
+    fi
+}
 
-    while ! diff "$TMP_BASHRC_FILE" "$PREVIOUS_TMP_BASHRC_FILE" > /dev/null 2>&1; do
-        cp "$TMP_BASHRC_FILE" "$PREVIOUS_TMP_BASHRC_FILE"
-        include-bashrc-sources "$TMP_BASHRC_FILE"
+
+make-func-files() {
+    # .func files are saved in $TMP_DIR. When run (only once each), they
+    # output a mangled string that marks the order in which the corresponding
+    # snipet code should be included in .bashrc.
+    # The corresponding .tmp files contain the code to include devoid of any
+    # nested includes.
+    local DIR=""
+    local BASE=""
+    local FUNC_FILE=""
+    local BASHRC_FILE=""
+    local FUNC_NAME=""
+    local i="    "
+
+    for file in $TMP_DEPS; do
+        BASE=$(basename $file)
+        FUNC_NAME=$(mangle-name $file $SCRIPT_DIR)
+        FUNC_FILE="$TMP_DIR/$FUNC_NAME.func"
+        BASHRC_FILE="$TMP_DIR/$FUNC_NAME.tmp"
+
+        # Build $FUNC_FILE
+        echo "$FUNC_NAME() {" > "$FUNC_FILE"
+    
+        echo "${i}if [ -z \$var_$FUNC_NAME ]; then" >> "$FUNC_FILE"
+        preprocess-source-file $file >> "$FUNC_FILE"
+        echo "" >> "$FUNC_FILE"
+        echo "${i}${i}echo \"\"" >> "$FUNC_FILE"
+        echo "${i}${i}var_$FUNC_NAME=1" >> "$FUNC_FILE"
+        echo "echo $FUNC_NAME" >> "$FUNC_FILE"
+        echo "${i}fi" >> "$FUNC_FILE"
+        echo "}" >> "$FUNC_FILE"
+
+        # Build $BASHRC_FILE
+        cat $file <(echo) | sed -E "s%^\s*(\.|source)\s+\S+$%%" > "$BASHRC_FILE"
     done
+}
+
+make-tmp-bashrc() {
+    rm -f "$TMP_BASHRC_FILE"
+
+    # Make sure all paths will be resolved
+    for file in $(find "$TMP_DIR" -type f | grep "\.func$"); do
+        preprocess-source-file $file >> "$TMP_BASHRC_FILE"
+    done
+
+    # Add entry point to run all nested name-mangled wrapper functions
+    echo $(mangle-name "$BASHRC_DIR/index.bashrc" $SCRIPT_DIR) >> "$TMP_BASHRC_FILE"
+
+    # Cache the order of execution of the wrapper functions
+    mv "$TMP_BASHRC_FILE" "$PREVIOUS_TMP_BASHRC_FILE"
+    source "$PREVIOUS_TMP_BASHRC_FILE" > "$TMP_BASHRC_FILE"
+    mv "$TMP_BASHRC_FILE" "$PREVIOUS_TMP_BASHRC_FILE"
+
+    # Build .bashrc in order, then delete tmp files
+    for func in $(cat "$PREVIOUS_TMP_BASHRC_FILE"); do
+        cat "$TMP_DIR/$func.tmp" >> "$TMP_BASHRC_FILE"
+        rm "$TMP_DIR/$func.tmp" "$TMP_DIR/$func.func"
+    done
+
+    # Clean up .bashrc (remove comments and multiline blanks)
+    mv "$TMP_BASHRC_FILE" "$PREVIOUS_TMP_BASHRC_FILE"
+    sed -E -e "/^\s*#/d;" -e "N;/^\n$/D;P;D" "$PREVIOUS_TMP_BASHRC_FILE" > "$TMP_BASHRC_FILE" 
 }
 
 SCRIPT_DIR=$( find-script-dir ${BASH_SOURCE[0]} )
@@ -101,14 +161,14 @@ set-avatar $1
 TMP_DEPS=$(collect-deps "$MAIN_FILE")
 
 make-index-files
+make-func-files
 make-tmp-bashrc
 
 cat "$TMP_BASHRC_FILE" | sed  "/^\s*#/d"
 
 unset get-deps _collect-deps collect-deps
-unset set-avatar
-unset include-bashrc-sources make-index-files
-unset make-tmp-bashrc
+unset set-avatar preprocess-source-file
+unset make-index-files make-func-files make-tmp-bashrc
 
 unset AVATAR
 unset SCRIPT_DIR BASHRC_DIR TMP_DIR
