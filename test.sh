d5f26152ca377ef7cbf4cda04e8c44dadf1a75cb
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-05-26 00:53:11 +0200

    ✅ test(task): keep only failed report

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-tests=$(find . | grep "__tests__/.*\.bats$" )
+tests=$(find . | grep "__tests__/.*\.bats$")
 
 for test in ${tests[@]}; do
-    # bats -F pretty "$test" | sed -e '/ ✓ /d' | sed -Ee ':a;$!N;s/\n([0-9])/\1/;ta;P;D'
-    bats "$test"
+    bats -F pretty "$test" | sed -e '/ ✓ /d' -e '/TASKRC override/d' -e '/TASKDATA override/d' | sed -Ee ':a;$!N;s/\n([0-9])/\1/;ta;P;D'
 done
