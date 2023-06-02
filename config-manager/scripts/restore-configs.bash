5adf3260ab18b063d504fc0365c5d18a630b53c4
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-06-02 22:56:37 +0200

    ✨ feat(tilix): compose profiles
    When Ctrl Alt T, launch a custom tilix layout with custom commands.

--- config-manager/scripts/restore-configs.bash
+++ config-manager/scripts/restore-configs.bash
@@ -2,3 +2,11 @@
 
 # Gnome config
 build-gnomerc | dconf load /org/gnome/
+
+# Tilix config
+build-tilixrc | dconf load /com/gexperts/Tilix/
+
+TILIX_DIR=$(find-script-dir ${BASH_SOURCE[0]})/../tilixrc
+
+mkdir -p "/home/jason/.config/tilix"
+cp "$TILIX_DIR/workspaces/task-workspace.json" "/home/jason/.config/tilix/"
