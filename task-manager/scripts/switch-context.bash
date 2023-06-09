8f9bbfbc883beb178a28600529da5f6c108e9bdb
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-06-09 10:55:40 +0200

    🐛 fix(task): set :selecting pseudo context
    tmp_context setting was moved around so as not to be void.
    This fixes autocontext when starting selection.
    
    Also autoformatted file on save.

--- task-manager/scripts/switch-context.bash
+++ task-manager/scripts/switch-context.bash
@@ -2,17 +2,17 @@
 
 # Get current task context and pick another in context list.
 # Currently just cycling through. A better heuristic should be
-# devised. 
+# devised.
 
 context=$(task _get rc.context)
 context=${context:-none}
-count=$(task +PENDING count 2> /dev/null)
+count=$(task +PENDING count 2>/dev/null)
 _SCRIPT_DIR=$(find-script-dir ${BASH_SOURCE[0]})
 _CONTEXTS_DIR="$_SCRIPT_DIR/../../task-manager/taskrc/contexts"
 
 _cycle-context() {
     cycle-context $(sed -E "s/:selecting$//" <<<"$context") "$_CONTEXTS_DIR"
-    count=$(task +PENDING count 2> /dev/null)
+    count=$(task +PENDING count 2>/dev/null)
     context=$(task _get rc.context)
     context=${context:-none}
     if [ $count = 0 ]; then
@@ -33,7 +33,7 @@ if [ -z $1 ]; then
 fi
 
 if [ $1 != workon ]; then
-    task +workon mod -workon -parent 2> /dev/null
+    task +workon mod -workon -parent 2>/dev/null
 fi
 
 if [ $1 = selecting ]; then
@@ -42,18 +42,18 @@ if [ $1 = selecting ]; then
     else
         rcfile=~/.taskrc
 
-        if ! grep "\.$tmp_context\." "$rcfile" > /dev/null 2>&1; then
+        tmp_context="$context:selecting"
+
+        if ! grep "\.$tmp_context\." "$rcfile" >/dev/null 2>&1; then
             echo "context.$tmp_context.read  =" \
                 $(sed -E "/^context\.$context\.read/!d;s/context\.$context\.read=(.*)/\1/" "$rcfile") \
                 $(sed -E "/^context\.selecting\.read/!d;s/context\.selecting\.read=(.*)/\1/" "$rcfile") \
-                >> "$rcfile"
+                >>"$rcfile"
             echo "context.$tmp_context.write =" \
                 $(sed -E "/^context\.$context\.write/!d;s/context\.$context\.write=(.*)/\1/" "$rcfile") \
                 $(sed -E "/^context\.selecting\.write/!d;s/context\.selecting\.write=(.*)/\1/" "$rcfile") \
-                >> "$rcfile"
+                >>"$rcfile"
         fi
-
-        tmp_context="$context:selecting"
     fi
 
     task context $tmp_context
