c3942e57eb3112f916e59441adce906c12cba461
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-06-10 08:04:48 +0200

    🐛 fix(task): don't crash on missing $1 or $2
    Also safety check on subtask_of which must be task_uuid.
    
    Also remove workon and selected from sex-subtask on remove.

--- task-manager/scripts/subtasking/remove-subtask.bash
+++ task-manager/scripts/subtasking/remove-subtask.bash
@@ -5,18 +5,20 @@
 
 source strict-mode
 
+set +u
 if [ -z "$1" ]; then
     echo "No task to remove subtasks from" >&2
     exit 1
 fi
 
 if [ -z "$2" ]; then
-    task_id=$(next_task-id)
+    task_id=$(next-task-id)
     subtask_id="$1"
 else
     task_id="$1"
     subtask_id="$2"
 fi
+set -u
 
 if [ -z $task_id ]; then
     echo "No task to remove subtasks from" >&2
@@ -28,6 +30,13 @@ subtask_id=$(task _get $subtask_id.id)
 task_uuid=$(task _get $task_id.uuid)
 subtask_uuid=$(task _get $subtask_id.uuid)
 
+parent_uuid=$(task _get $subtask_id.subtask_of)
+
+if [ -z $parent_uuid ] || [ $parent_uuid != $task_uuid ]; then
+    echo "Safety check failed: subtask_of $parent_uuid != task_uuid $task_uuid" >&2
+    exit 1
+fi
+
 previous_subtasks=$(task _get $task_id.subtasks)
 
 # Remove from subtasks array
@@ -38,7 +47,7 @@ new_subtasks=$(echo $subtasks | sed -e 's/ /,/g')
 
 # Update task
 task $task_id mod subtasks:$new_subtasks
-task $subtask_id mod subtask_of:
+task $subtask_id mod subtask_of: -workon -selected
 
 # Print pending tasks
 task
