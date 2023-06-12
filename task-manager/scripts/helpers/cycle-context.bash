43dca97cf533f5a9cd7097c8cf9f1b981821f556
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-06-12 13:46:22 +0200

    🐛 fix(task): cycle out of workon to none
    When workon context is set, switch to none.
    
    Workon is a valid temporary context, but corresponds to
    no static context in particular.

--- task-manager/scripts/helpers/cycle-context.bash
+++ task-manager/scripts/helpers/cycle-context.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Switch $1 context for another. Currently just cycle through contexts.
+# If $1 is found within context directory $2, then switch to next context in directory,
+# or back to first if $1 is last.
+# If $1 is workon, switch to none
+# Else let task handle the error.
 
 # exit on first error and return errno
 set -e
@@ -18,8 +22,7 @@ if ! [[ "$1" =~ ^[[:alpha:]][[:alnum:]]*$ ]]; then
     task context $1
 fi
 
-for context in "${contexts[@]}";
-do
+for context in "${contexts[@]}"; do
     i=$((i + 1))
     if [ $1 = $context ]; then
         context=${contexts[$i]}
@@ -30,6 +33,8 @@ done
 if [ ! -z $context ]; then
     if [[ $i < "${#contexts[@]}" ]]; then
         task context $context
+    elif [ $1 = workon ]; then
+        task context none
     else
         # Pass along $1 and let `task` handle the error
         task context $1
