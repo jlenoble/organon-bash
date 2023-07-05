227f7d8f1a12f052e4206dc90734fda2c0f34ce9
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-07-05 19:58:21 +0200

    ✨ feat(nasm-scripts): add mem16 src

--- nasm-scripts/gen-instruction.bash
+++ nasm-scripts/gen-instruction.bash
@@ -6,6 +6,9 @@ op_mode=${3:-16}
 
 imm8="0 12 16 150"
 imm16="0 12 16 150 1000 32768 50000"
+mem16="0 100 10000"
+mem32="0 100 10000 1000000 100000000"
+mem64="0 100 10000 1000000 100000000 10000000000 1000000000000"
 
 echo "[bits $op_mode]"
 
@@ -28,17 +31,24 @@ case $op_mode in
         done
     done
 
-    # r8, imm
+    # r8, imm8
     for src in $imm8; do
         for dest in $r8; do
             echo "        $mnemonics $dest, $src"
         done
     done
 
-    # r16, imm
+    # r16, imm16
     for src in $imm16; do
         for dest in $r16; do
             echo "        $mnemonics $dest, $src"
+        done
+    done
+
+    # r16, mem16
+    for src in $mem16; do
+        for dest in $r16; do
+            echo "        $mnemonics $dest, [$src]"
         done
     done
     ;;
