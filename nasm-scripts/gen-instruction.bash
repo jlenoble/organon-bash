6cbc73ef8745bb07c34bbd0aaf9afce3d6f6ba19
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2023-07-05 15:32:47 +0200

    ✨ feat(nasm-scripts): add immediate src
    src was limited to general purpose registers. Now may be an immediate
    value.

--- nasm-scripts/gen-instruction.bash
+++ nasm-scripts/gen-instruction.bash
@@ -4,6 +4,9 @@ mnemonics=${1:-mov}
 n_operands=${2:-2}
 op_mode=${3:-16}
 
+imm8="0 12 16 150"
+imm16="0 12 16 150 1000 32768 50000"
+
 echo "[bits $op_mode]"
 
 case $op_mode in
@@ -11,13 +14,29 @@ case $op_mode in
     r8="$(list-registers lo) $(list-registers hi)"
     r16="$(list-registers 16)"
 
+    # r8, r8
     for src in $r8; do
         for dest in $r8; do
             echo "        $mnemonics $dest, $src"
         done
     done
 
+    # r16, r16
     for src in $r16; do
+        for dest in $r16; do
+            echo "        $mnemonics $dest, $src"
+        done
+    done
+
+    # r8, imm
+    for src in $imm8; do
+        for dest in $r8; do
+            echo "        $mnemonics $dest, $src"
+        done
+    done
+
+    # r16, imm
+    for src in $imm16; do
         for dest in $r16; do
             echo "        $mnemonics $dest, $src"
         done
