d67e216fc298798343d485a243fc5859e6533235
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2024-05-21 16:37:29 +0200

    ✨ feat(backup): backup/restore scripts

---
+++ setup-manager/backup.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ENTRY_FILE_STEM=$1
+
+# Cannot guess ENTRY_FILE_STEM, must be provided
+if [ -z $ENTRY_FILE_STEM ]; then
+	echo "[ERROR] backup: ENTRY_FILE_STEM must be provided as \$1" >&2
+	exit 1
+fi
+
+SCRIPT_DIR="$(find-script-dir ${BASH_SOURCE[0]})"
+TMP_DIR="$SCRIPT_DIR/.tmp"
+
+build-rc _ organonrc "$SCRIPT_DIR" "backup/$ENTRY_FILE_STEM" backup 2>&1 >/dev/null
+
+# Exec custom built script
+$TMP_DIR/organonrc.tmp
+
+unset SCRIPT_DIR TMP_DIR ENTRY_FILE_STEM
