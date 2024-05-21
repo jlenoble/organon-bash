5633ecc13fd1593e892682d553a46b199128833d
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2024-05-21 10:42:58 +0200

    ✨ feat(bootstrap): first install bootstrap script
    After a clean install of bookworm, this script helps installing
    and configuring rclone and git (manual interventions are required
    though here and there for security's sake).
    
    After running it, install.sh and build.sh in organon-bash should
    suffice to install and configure the rest.

---
+++ bootstrap-bookworm.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# After clean install, first configure Google Drive access via rclone.
+# > sudo apt-get install rclone
+# You will need to recover client ID, secret, password and salt to complete config.
+# > sudo apt-get install keepass2
+# Remotes are expected to be called GoogleDrive and GoogleDriveEncrypted.
+# > rclone config
+
+# Execute locally https://github.com/jlenoble/organon-bash/blob/main/bootstrap-bookworm.sh
+# (this very file) to configure secure access to github and clone all bash scripts
+if [ ! -d ~/Projets/organon-bash ]; then
+	sudo apt-get install git
+	rclone sync GoogleDriveEncrypted:BackUp/.gitconfig ~
+	rclone sync GoogleDriveEncrypted:BackUp/.ssh ~/.ssh
+	chmod 700 ~/.ssh
+	chmod 600 ~/.ssh/*
+	mkdir -p ~/Projets
+	cd ~/Projets
+	git clone git@github.com:jlenoble/organon-bash
+	cd ~
+else
+	echo "~/Projets/organon-bash already exists; git should already be installed and configured"
+fi
