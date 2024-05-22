ce862b5af9055cf2e1affb996240abb72693fbda
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2024-05-22 12:30:27 +0200

    ✨ feat(scripts): launch and kill verdaccio

---
+++ misc-scripts/verdaccio.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm use 20
+cd ~/Projets/vault
+rush verdaccio
