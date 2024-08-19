1720423df904171e4d80c53c89f1a7aaa076b73e
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2024-08-19 15:00:00 +0200

    ✨ feat(misc): superProductivity launcher

---
+++ misc-scripts/super-productivity.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ~/Projets/super-productivity
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm use 20
+
+npm start &
+ng serve
