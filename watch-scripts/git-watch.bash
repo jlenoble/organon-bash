80a5117f2459dd3d9232557dd5626b5b1283c7c0
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2024-08-23 16:00:02 +0200

    ✨ feat(watch): watch more git logs
    add an argument to watch distinct git logs.

--- watch-scripts/git-watch.bash
+++ watch-scripts/git-watch.bash
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
-GIT_DIR=$(find-script-dir ${BASH_SOURCE[0]})/../.git
-GIT_HEAD_FILE=$GIT_DIR/logs/HEAD
+SCRIPT_DIR="$(find-script-dir ${BASH_SOURCE[0]})"
+PROJECT_DIR="$SCRIPT_DIR/../.."
+target="${1:-vault}"
+GIT_DIR="$PROJECT_DIR/$target/.git"
+GIT_HEAD_FILE="$GIT_DIR/logs/HEAD"
 
-cd $GIT_DIR/..
+case $target in
+organon-bash | organon-wagtail | vault)
+    cd "$GIT_DIR/.."
 
-execute() {
-    clear
-    echo
-    git --no-pager log -n 10 --reverse
-}
+    execute() {
+        clear
+        echo
+        git --no-pager log -n 10 --reverse
+    }
 
-execute
-
-while sleep-until-modified $GIT_HEAD_FILE; do
     execute
-done
+
+    while sleep-until-modified "$GIT_HEAD_FILE"; do
+        execute
+    done
+    ;;
+
+*) ;;
+esac
+
+unset target SCRIPT_DIR GIT_TARGET GIT_HEAD_FILE
