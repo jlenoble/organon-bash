f9210c1184d6914980ba2b504c52da35ff9ffb92
Jason Lenoble <20752619+jlenoble@users.noreply.github.com>
2025-02-16 03:17:30 +0100

    🐛 fix(restore): make paths consistent with gnome automount

--- bootstrap-from-backup-drive.sh
+++ bootstrap-from-backup-drive.sh
@@ -20,9 +20,28 @@
 MOUNTDRIVE=$1
 
 if [ -z "$MOUNTDRIVE" ]; then
-	echo "You must specify as first arg the device to be mounted (e.g. /dev/sdc1)" 1>&2
+	echo "You must specify as first arg the device to be mounted (e.g. /dev/sdc1).
+	You should find it among the devices below or it may not be plugged, or if working with WSL2
+	you must first expose the drive using Powershell (open this script for detailed instructions)" 1>&2
+
+	# Help find the external drive
+	lsblk
+
+	# sdc      8:32   0 931.5G  0 disk
+	# └─sdc1   8:33   0 931.5G  0 part
+
+	# Now bail out
 	exit 1
 fi
+
+MEDIA_DIR=/media/$USER
+EXT_DRIVE_NAME=T7
+BACKUP_DIR_NAME=BackUp
+BACKUP_DRIVE_PATH="$MEDIA_DIR/$EXT_DRIVE_NAME"
+BACKUP_PATH="$BACKUP_DRIVE_DIR/$BACKUP_DIR_NAME"
+
+# Make sure the dir for the drive mount exists
+sudo mkdir -p "$BACKUP_DRIVE_PATH"
 
 # Make the system up-to-date
 sudo apt update
@@ -31,25 +50,18 @@ sudo apt upgrade
 # Add required packages
 sudo apt install cryptsetup git openssh-client
 
-# Find the  external drive
-lsblk
-
-# sdc      8:32   0 931.5G  0 disk
-# └─sdc1   8:33   0 931.5G  0 part
-
 # Unlock the LUKS encryption
 sudo cryptsetup open "$MOUNTDRIVE" luks-drive
 
 # Mount the ext4 partition
-sudo mkdir -p /mnt/backup_drive
-sudo mount /dev/mapper/luks-drive /mnt/backup_drive
+sudo mount /dev/mapper/luks-drive "$BACKUP_DRIVE_PATH"
 
 # Recover ssh public keys
-cp -r /mnt/backup_drive/BackUp/jason/.ssh/ ~/
+cp -r "$BACKUP_PATH/$USER/.ssh/" ~/
 chmod 700 .ssh
 
 # Recover git config
-cp -r /mnt/backup_drive/BackUp/jason/.gitconfig ~/
+cp -r "$BACKUP_PATH/$USER/.gitconfig" ~/
 chmod 600 .gitconfig
 
 # Clone organon-bash
